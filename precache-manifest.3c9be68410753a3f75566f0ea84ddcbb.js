11fe9d970d9dc23385b63d7c6d774da453da1904
piksel <piksel@users.noreply.github.com>
2020-07-12 01:21:14 +0000

    deploy: b9db2359c773106ecc6e127bdd16c3007f749579

---
+++ precache-manifest.3c9be68410753a3f75566f0ea84ddcbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2332bb9ed179f615036a34bbf063681c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94cc4337a95485454c47",
+    "url": "/static/css/main.f084d9cd.chunk.css"
+  },
+  {
+    "revision": "c6be1d09ecbb4a52fc92",
+    "url": "/static/js/2.bf02fdc9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bf02fdc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94cc4337a95485454c47",
+    "url": "/static/js/main.3f05801f.chunk.js"
+  },
+  {
+    "revision": "4240a00f64155d64b5c9",
+    "url": "/static/js/runtime-main.6edae3b2.js"
+  }
+]);
